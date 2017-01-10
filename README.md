c8dea5ca08bbc9a6d48366076b3c71e5d6a6cf9f
Neil Magee <info@neilmagee.com>
2017-01-10 10:41:15 +0000

    Added a work around task

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nested-sass-sourcemaps
 Reduced test case to break Gulp SASS Sourcemaps  
-Using gulp-autoprefixer with scss in nested directories causes sass sourcemaps to break
+Using gulp-autoprefixer with scss in nested directories causes sass sourcemaps to break  
+See gulpfile.js for a work around to try and get a sourcemap file that has the correct mapping of sources to enable debugging in the browser.
--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('prefix', function (){
         .pipe(prefix())
         .pipe(sourcemaps.write('.', {
             includeContent: false,
-            sourceRoot: '../src/scss'
+            sourceRoot: '../../../src/scss'
         }))
         .pipe(gulp.dest(paths.styles.dest + '/prefix'));
 });
@@ -29,9 +29,25 @@ gulp.task('noPrefix', function (){
         .pipe(sass())
         .pipe(sourcemaps.write('.', {
             includeContent: false,
-            sourceRoot: '../src/scss'
+            sourceRoot: '../../../src/scss'
         }))
         .pipe(gulp.dest(paths.styles.dest + '/noPrefix'));
 });
 
-gulp.task('default', ['noPrefix', 'prefix']);
+gulp.task('workAround', function (){
+    return gulp.src(paths.styles.files)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write({
+            includeContent: false
+        }))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(prefix())
+        .pipe(sourcemaps.write('.', {
+            includeContent: false,
+            destPath: paths.styles.dest + '/workAround'
+        }))
+        .pipe(gulp.dest(paths.styles.dest + '/workAround'));
+});
+
+gulp.task('default', ['noPrefix', 'prefix', 'workAround']);
